8ec5e30520763e51b0e9f38200be3d1daa452ca9
Fabio Miranda <fabiomiranda@MacBook-Air-de-Fabio.local>
2022-12-07 10:49:32 -0300

    fabio teste

---
+++ 05_firststepsinjava/rest-with-spring-boot-and-java-erudio.git/src/test/java/com/fabio/demo/StartupTests.java
@@ -0,0 +1,13 @@
+package com.fabio.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
